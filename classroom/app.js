f28d54eb89b709e77253916049deff30f550ad32
madewithcode <ahuja.software@gmail.com>
2017-12-02 23:18:57 -0500

    preparing workspace for modules

--- classroom/app.js
+++ classroom/app.js
@@ -1,8 +1,1 @@
-/// <reference path="circleMath.ts"/>
-/// <reference path="rectangleMath.ts"/>
-var CircleMath = MyMath.Circle;
-const PI = 2.99;
-console.log(MyMath.calculateRectangle(10, 30));
-console.log(CircleMath.calculateCircumference(3));
-console.log(PI);
 //# sourceMappingURL=app.js.map
