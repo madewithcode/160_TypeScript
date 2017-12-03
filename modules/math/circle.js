e3b1ed2b116b35ea19b8a408179f1c1788d76e03
madewithcode <ahuja.software@gmail.com>
2017-12-03 00:59:25 -0500

    modules 2

---
+++ modules/math/circle.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Vinay on 12/2/17.
+ */
+"use strict";
+exports.PI = 3.14;
+function calculateCircumference(diameter) {
+    return diameter * exports.PI;
+}
+exports.calculateCircumference = calculateCircumference;
+//# sourceMappingURL=circle.js.map
---
+++ modules/math/rectangle.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Vinay on 12/2/17.
+ */
+"use strict";
+function calculateRectangle(width, length) {
+    return width * length;
+}
+exports.calculateRectangle = calculateRectangle;
